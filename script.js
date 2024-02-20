51e72daa6b60d78314bb7814ed7f41df45fd8f32
Alexander Vielma <vielmaa1804@gmail.com>
2024-02-20 09:21:39 -0400

    Finish all functionality

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const PW = document.querySelector("#password");
 const CONFIRM_PW = document.querySelector("#confirm-pw")
 const bothIcons = document.querySelectorAll(".material-symbols-outlined");
+const PW_INPUTS = document.querySelectorAll(".pass")
+const FORM = document.querySelector("#form");
+const ERROR_SPAN = document.querySelectorAll(".error-msg");
 
-console.log(bothIcons)
 
 const toggleEyeVisibility = (icon) => {
     let visible = "visibility";
@@ -38,10 +40,40 @@ const togglePasswordVisibility = (input) => {
 
 bothIcons.forEach(icon => icon.addEventListener("click", (e) => {
     const labelAttribute = e.target.parentElement.parentElement.getAttribute("for");
-
-
     togglePasswordVisibility(labelAttribute)
     toggleEyeVisibility(e.target)
 }))
 
 
+let password = [];
+let confirmPassword = [];
+
+const getPasswordValues = (input) => {
+    if (input.target.getAttribute("id") == "password"){
+        if (password.length > 0){
+            password.pop()
+            password.push(input.target.value)
+        } else {
+            password.push(input.target.value)
+        }
+    } else {
+        if (confirmPassword.length > 0) {
+            confirmPassword.pop();
+            confirmPassword.push(input.target.value)
+        } else {
+            confirmPassword.push(input.target.value)
+        }
+    }
+} 
+
+PW_INPUTS.forEach(input => input.addEventListener("input", (e) => {
+   getPasswordValues(e);
+   ERROR_SPAN.forEach(span => span.classList.remove("error"))
+}))
+
+FORM.addEventListener("submit", (e) => {
+    if (PW.value !== CONFIRM_PW.value){
+        ERROR_SPAN.forEach(span => span.classList.add("error"))
+        e.preventDefault()
+    }
+})
